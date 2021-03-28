14338d311f3010c2b0515cb7d25f8b24b6e4b855
shenmishajing <shenmishajing@Gmail.com>
2021-03-28 12:45:12 +0800

    add upload run config feature

--- tools/train.py
+++ tools/train.py
@@ -19,55 +19,55 @@ from mmdet.utils import collect_env, get_root_logger
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Train a detector')
-    parser.add_argument('config', help='train config file path')
-    parser.add_argument('--work-dir', help='the dir to save logs and models')
+    parser = argparse.ArgumentParser(description = 'Train a detector')
+    parser.add_argument('config', help = 'train config file path')
+    parser.add_argument('--work-dir', help = 'the dir to save logs and models')
     parser.add_argument(
-        '--resume-from', help='the checkpoint file to resume from')
+        '--resume-from', help = 'the checkpoint file to resume from')
     parser.add_argument(
         '--no-validate',
-        action='store_true',
-        help='whether not to evaluate the checkpoint during training')
+        action = 'store_true',
+        help = 'whether not to evaluate the checkpoint during training')
     group_gpus = parser.add_mutually_exclusive_group()
     group_gpus.add_argument(
         '--gpus',
-        type=int,
-        help='number of gpus to use '
-        '(only applicable to non-distributed training)')
+        type = int,
+        help = 'number of gpus to use '
+               '(only applicable to non-distributed training)')
     group_gpus.add_argument(
         '--gpu-ids',
-        type=int,
-        nargs='+',
-        help='ids of gpus to use '
-        '(only applicable to non-distributed training)')
-    parser.add_argument('--seed', type=int, default=None, help='random seed')
+        type = int,
+        nargs = '+',
+        help = 'ids of gpus to use '
+               '(only applicable to non-distributed training)')
+    parser.add_argument('--seed', type = int, default = None, help = 'random seed')
     parser.add_argument(
         '--deterministic',
-        action='store_true',
-        help='whether to set deterministic options for CUDNN backend.')
+        action = 'store_true',
+        help = 'whether to set deterministic options for CUDNN backend.')
     parser.add_argument(
         '--options',
-        nargs='+',
-        action=DictAction,
-        help='override some settings in the used config, the key-value pair '
-        'in xxx=yyy format will be merged into config file (deprecate), '
-        'change to --cfg-options instead.')
+        nargs = '+',
+        action = DictAction,
+        help = 'override some settings in the used config, the key-value pair '
+               'in xxx=yyy format will be merged into config file (deprecate), '
+               'change to --cfg-options instead.')
     parser.add_argument(
         '--cfg-options',
-        nargs='+',
-        action=DictAction,
-        help='override some settings in the used config, the key-value pair '
-        'in xxx=yyy format will be merged into config file. If the value to '
-        'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
-        'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
-        'Note that the quotation marks are necessary and that no white space '
-        'is allowed.')
+        nargs = '+',
+        action = DictAction,
+        help = 'override some settings in the used config, the key-value pair '
+               'in xxx=yyy format will be merged into config file. If the value to '
+               'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
+               'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
+               'Note that the quotation marks are necessary and that no white space '
+               'is allowed.')
     parser.add_argument(
         '--launcher',
-        choices=['none', 'pytorch', 'slurm', 'mpi'],
-        default='none',
-        help='job launcher')
-    parser.add_argument('--local_rank', type=int, default=0)
+        choices = ['none', 'pytorch', 'slurm', 'mpi'],
+        default = 'none',
+        help = 'job launcher')
+    parser.add_argument('--local_rank', type = int, default = 0)
     args = parser.parse_args()
     if 'LOCAL_RANK' not in os.environ:
         os.environ['LOCAL_RANK'] = str(args.local_rank)
@@ -129,7 +129,7 @@ def main():
     # init the logger before other steps
     timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
     log_file = osp.join(cfg.work_dir, f'{timestamp}.log')
-    logger = get_root_logger(log_file=log_file, log_level=cfg.log_level)
+    logger = get_root_logger(log_file = log_file, log_level = cfg.log_level)
 
     # init the meta dict to record some important information such as
     # environment info and seed, which will be logged
@@ -150,15 +150,15 @@ def main():
     if args.seed is not None:
         logger.info(f'Set random seed to {args.seed}, '
                     f'deterministic: {args.deterministic}')
-        set_random_seed(args.seed, deterministic=args.deterministic)
+        set_random_seed(args.seed, deterministic = args.deterministic)
     cfg.seed = args.seed
     meta['seed'] = args.seed
     meta['exp_name'] = osp.basename(args.config)
 
     model = build_detector(
         cfg.model,
-        train_cfg=cfg.get('train_cfg'),
-        test_cfg=cfg.get('test_cfg'))
+        train_cfg = cfg.get('train_cfg'),
+        test_cfg = cfg.get('test_cfg'))
 
     datasets = [build_dataset(cfg.data.train)]
     if len(cfg.workflow) == 2:
@@ -169,18 +169,22 @@ def main():
         # save mmdet version, config file content and class names in
         # checkpoints as meta data
         cfg.checkpoint_config.meta = dict(
-            mmdet_version=__version__ + get_git_hash()[:7],
-            CLASSES=datasets[0].CLASSES)
+            mmdet_version = __version__ + get_git_hash()[:7],
+            CLASSES = datasets[0].CLASSES)
+    for hook in cfg.log_config.hooks:
+        if hook.type == 'WandbLoggerHook':
+            hook.init_kwargs.config = copy.deepcopy(cfg)
+            hook.init_kwargs.config.log_config.pop('hooks')
     # add an attribute for visualization convenience
     model.CLASSES = datasets[0].CLASSES
     train_detector(
         model,
         datasets,
         cfg,
-        distributed=distributed,
-        validate=(not args.no_validate),
-        timestamp=timestamp,
-        meta=meta)
+        distributed = distributed,
+        validate = (not args.no_validate),
+        timestamp = timestamp,
+        meta = meta)
 
 
 if __name__ == '__main__':
