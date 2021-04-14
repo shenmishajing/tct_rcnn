6d28d890827249e52950feac6987cc50c73e2d03
shenmishajing <shenmishajing@Gmail.com>
2021-04-14 15:11:55 +0800

    add val loss feature

--- configs/faster_rcnn/faster_rcnn_r50_fpn_1x_tct.py
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_1x_tct.py
@@ -9,6 +9,7 @@ log_config = dict(
     hooks = [
         dict(type = 'TextLoggerHook'),
         dict(type = 'WandbLoggerHook',
+             with_step = False,
              init_kwargs = dict(project = 'tct',
                                 name = 'faster_rcnn_r50_fpn_1x_tct',
                                 tags = ['mmdetection', 'tct', 'faster_rcnn', 'r50', '1x']))])
