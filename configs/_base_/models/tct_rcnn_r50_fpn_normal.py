3dc2464ad14a436884d10cb6e8566ae00331f21f
shenmishajing <shenmishajing@Gmail.com>
2021-04-23 15:57:41 +0800

    fix tct_rcnn_normal low mAP bug

--- configs/_base_/models/tct_rcnn_r50_fpn_normal.py
+++ configs/_base_/models/tct_rcnn_r50_fpn_normal.py
@@ -94,9 +94,9 @@ model = dict(
                 pos_weight = -1,
                 debug = False),
             test_cfg = dict(
-                score_thr = 0.5,
+                score_thr = 0.05,
                 nms = dict(type = 'nms', iou_threshold = 0.5),
-                max_per_img = 1)
+                max_per_img = 100)
             # soft-nms is also supported for rcnn testing
             # e.g., nms=dict(type='soft_nms', iou_threshold=0.5, min_score=0.05)
         ),
