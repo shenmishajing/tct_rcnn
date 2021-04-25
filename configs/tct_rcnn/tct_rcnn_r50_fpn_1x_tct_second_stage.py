434deaf662a8566121bf7e47ecc0d1e5b6c59276
shenmishajing <shenmishajing@Gmail.com>
2021-04-25 14:09:04 +0800

    use Run_32 checkpoint

--- configs/tct_rcnn/tct_rcnn_r50_fpn_1x_tct_second_stage.py
+++ configs/tct_rcnn/tct_rcnn_r50_fpn_1x_tct_second_stage.py
@@ -3,7 +3,7 @@ _base_ = [
     '../_base_/datasets/tct_detection.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
-load_from = '/data/zhengwenhao/Result/TCT-RCNN/checkpoints/Run_31/best_bbox_mAP_50.pth'
+load_from = '/data/zhengwenhao/Result/TCT-RCNN/checkpoints/Run_32/best_bbox_mAP_50.pth'
 custom_hooks = []
 
 log_config = dict(
