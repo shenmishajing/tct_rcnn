95307dac0d9b0e72880d2b91a7ad0a4130b17dba
shenmishajing <shenmishajing@Gmail.com>
2021-04-26 10:18:16 +0800

    use base in tct_detection_normal.py

--- configs/_base_/datasets/tct_detection_normal.py
+++ configs/_base_/datasets/tct_detection_normal.py
@@ -1,52 +1,13 @@
-dataset_type = 'TCTDataset'
+_base_ = 'tct_detection.py'
 part = 'normal'
-data_root = 'data/tct/'
-img_norm_cfg = dict(
-    mean = [123.675, 116.28, 103.53], std = [58.395, 57.12, 57.375], to_rgb = True)
-train_pipeline = [
-    dict(type = 'LoadImageFromFile'),
-    dict(type = 'LoadAnnotations', with_bbox = True),
-    dict(type = 'Resize', img_scale = (1333, 800), keep_ratio = True),
-    dict(type = 'RandomFlip', flip_ratio = 0.5),
-    dict(type = 'Normalize', **img_norm_cfg),
-    dict(type = 'Pad', size_divisor = 32),
-    dict(type = 'DefaultFormatBundle'),
-    dict(type = 'Collect', keys = ['img', 'gt_bboxes', 'gt_labels']),
-]
-test_pipeline = [
-    dict(type = 'LoadImageFromFile'),
-    dict(
-        type = 'MultiScaleFlipAug',
-        img_scale = (1333, 800),
-        flip = False,
-        transforms = [
-            dict(type = 'Resize', keep_ratio = True),
-            dict(type = 'RandomFlip'),
-            dict(type = 'Normalize', **img_norm_cfg),
-            dict(type = 'Pad', size_divisor = 32),
-            dict(type = 'ImageToTensor', keys = ['img']),
-            dict(type = 'Collect', keys = ['img']),
-        ])
-]
+min_size = 16
 data = dict(
-    samples_per_gpu = 4,
-    workers_per_gpu = 4,
     train = dict(
-        type = dataset_type,
-        ann_file = data_root + 'annotations/train',
-        img_prefix = data_root + 'train2017/',
         part = part,
-        pipeline = train_pipeline),
+        min_size = min_size),
     val = dict(
-        type = dataset_type,
-        ann_file = data_root + 'annotations/val',
-        img_prefix = data_root + 'val2017/',
         part = part,
-        pipeline = test_pipeline),
+        min_size = min_size),
     test = dict(
-        type = dataset_type,
-        ann_file = data_root + 'annotations/test',
-        img_prefix = data_root + 'val2017/',
         part = part,
-        pipeline = test_pipeline))
-evaluation = dict(interval = 1, metric = 'bbox', save_best = 'bbox_mAP_50', classwise = True)
+        min_size = min_size))
