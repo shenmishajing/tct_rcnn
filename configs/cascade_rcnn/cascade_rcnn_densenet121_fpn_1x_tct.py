dbd40ba2f7fa51d3bc487bf848b0b8bba2e9480a
shenmishajing <shenmishajing@Gmail.com>
2021-05-20 16:30:50 +0800

    add faster rcnn use vgg16 as backbone add cascade rcnn use densenet121 as backbone, but it has bug now

---
+++ configs/cascade_rcnn/cascade_rcnn_densenet121_fpn_1x_tct.py
@@ -0,0 +1,73 @@
+_base_ = [
+    '../_base_/models/cascade_rcnn_densenet121_fpn.py',
+    '../_base_/datasets/tct_detection.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+
+optimizer = dict(lr = 0.005)
+
+model = dict(roi_head = dict(bbox_head = [
+    dict(
+        type = 'Shared2FCBBoxHead',
+        in_channels = 256,
+        fc_out_channels = 1024,
+        roi_feat_size = 7,
+        num_classes = 5,
+        bbox_coder = dict(
+            type = 'DeltaXYWHBBoxCoder',
+            target_means = [0., 0., 0., 0.],
+            target_stds = [0.1, 0.1, 0.2, 0.2]),
+        reg_class_agnostic = True,
+        loss_cls = dict(
+            type = 'CrossEntropyLoss',
+            use_sigmoid = False,
+            loss_weight = 1.0),
+        loss_bbox = dict(type = 'SmoothL1Loss', beta = 1.0,
+                         loss_weight = 1.0)),
+    dict(
+        type = 'Shared2FCBBoxHead',
+        in_channels = 256,
+        fc_out_channels = 1024,
+        roi_feat_size = 7,
+        num_classes = 5,
+        bbox_coder = dict(
+            type = 'DeltaXYWHBBoxCoder',
+            target_means = [0., 0., 0., 0.],
+            target_stds = [0.05, 0.05, 0.1, 0.1]),
+        reg_class_agnostic = True,
+        loss_cls = dict(
+            type = 'CrossEntropyLoss',
+            use_sigmoid = False,
+            loss_weight = 1.0),
+        loss_bbox = dict(type = 'SmoothL1Loss', beta = 1.0,
+                         loss_weight = 1.0)),
+    dict(
+        type = 'Shared2FCBBoxHead',
+        in_channels = 256,
+        fc_out_channels = 1024,
+        roi_feat_size = 7,
+        num_classes = 5,
+        bbox_coder = dict(
+            type = 'DeltaXYWHBBoxCoder',
+            target_means = [0., 0., 0., 0.],
+            target_stds = [0.033, 0.033, 0.067, 0.067]),
+        reg_class_agnostic = True,
+        loss_cls = dict(
+            type = 'CrossEntropyLoss',
+            use_sigmoid = False,
+            loss_weight = 1.0),
+        loss_bbox = dict(type = 'SmoothL1Loss', beta = 1.0, loss_weight = 1.0))
+]))
+
+data = dict(
+    samples_per_gpu = 3,
+    workers_per_gpu = 3)
+
+log_config = dict(
+    hooks = [
+        dict(type = 'TextLoggerHook'),
+        dict(type = 'WandbLoggerHook',
+             with_step = False,
+             init_kwargs = dict(project = 'tct',
+                                name = 'cascade_rcnn_densenet121_fpn_1x_tct',
+                                tags = ['mmdetection', 'tct', 'cascade_rcnn', 'densenet121', '1x']))])
