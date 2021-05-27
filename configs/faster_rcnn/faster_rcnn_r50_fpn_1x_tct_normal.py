5e4575c0681fad48984df22b2f32e8b3a4393c77
shenmishajing <shenmishajing@Gmail.com>
2021-05-27 16:27:20 +0800

    fix merge bug

--- configs/faster_rcnn/faster_rcnn_r50_fpn_1x_tct_normal.py
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_1x_tct_normal.py
@@ -1,6 +1,7 @@
 _base_ = [
     '../_base_/models/faster_rcnn_r50_fpn.py',
-    '../_base_/datasets/tct_detection_normal.py'
+    '../_base_/datasets/tct_detection_normal.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 model = dict(roi_head = dict(bbox_head = dict(num_classes = 1)))
 optimizer = dict(lr = 0.005)
