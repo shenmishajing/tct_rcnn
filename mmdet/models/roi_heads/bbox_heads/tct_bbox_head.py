6e5c724382d2e5e0de3af9d15be261bb97a8a4cc
shenmishajing <shenmishajing@Gmail.com>
2021-06-12 16:48:08 +0800

    add dynamic module feature

--- mmdet/models/roi_heads/bbox_heads/tct_bbox_head.py
+++ mmdet/models/roi_heads/bbox_heads/tct_bbox_head.py
@@ -17,50 +17,23 @@ class TCTBBoxHead(ConvFCBBoxHead):
                                                     \-> reg convs -> reg fcs -> reg
     """  # noqa: W605
 
+    def __init__(self,
+                 *args,
+                 **kwargs):
+        super(TCTBBoxHead, self).__init__(*args, **kwargs)
+        self.dynamic_fc = nn.Linear(self.in_channels, 2 * self.in_channels)
+
     def forward(self, x, normal_feats = None, batch_inds = None):
-        # shared part
-        if self.num_shared_convs > 0:
-            for conv in self.shared_convs:
-                x = conv(x)
-
-        if self.num_shared_fcs > 0:
-            if self.with_avg_pool:
-                x = self.avg_pool(x)
-
-            x = self.relu(self.shared_fcs[0](x.flatten(1)))
-            normal_feats = [torch.mean(normal_feat, dim = 0) if len(normal_feat.size()) == 4 else normal_feat for normal_feat in
-                            normal_feats]
-            normal_feats = torch.cat(normal_feats)
-            normal_feats = self.relu(self.shared_fcs[0](normal_feats.flatten(1)))
-            x_list = []
-            for i in range(len(normal_feats)):
-                x_list.append(2 * x[batch_inds == i] - normal_feats[i])
-            x = torch.cat(x_list)
-
-            for fc in self.shared_fcs[1:]:
-                x = self.relu(fc(x))
-        # separate branches
-        x_cls = x
-        x_reg = x
-
-        for conv in self.cls_convs:
-            x_cls = conv(x_cls)
-        if x_cls.dim() > 2:
-            if self.with_avg_pool:
-                x_cls = self.avg_pool(x_cls)
-            x_cls = x_cls.flatten(1)
-        for fc in self.cls_fcs:
-            x_cls = self.relu(fc(x_cls))
-
-        for conv in self.reg_convs:
-            x_reg = conv(x_reg)
-        if x_reg.dim() > 2:
-            if self.with_avg_pool:
-                x_reg = self.avg_pool(x_reg)
-            x_reg = x_reg.flatten(1)
-        for fc in self.reg_fcs:
-            x_reg = self.relu(fc(x_reg))
-
-        cls_score = self.fc_cls(x_cls) if self.with_cls else None
-        bbox_pred = self.fc_reg(x_reg) if self.with_reg else None
-        return cls_score, bbox_pred
+        x_list = []
+        for i in range(len(normal_feats)):
+            normal_feat = normal_feats[i]
+            if normal_feat.ndim == 4:
+                normal_feat = normal_feat.mean(0)
+            normal_feat = normal_feat.flatten(1).T
+            params = self.dynamic_fc(normal_feat).reshape(self.roi_feat_area, self.in_channels, 2)
+            cur_x = x[batch_inds == i].flatten(2).permute(0, 2, 1)
+            cur_x = cur_x.matmul(params[:, :, 0].T).matmul(params[:, :, 1]).permute(0, 2, 1).reshape(-1, self.in_channels,
+                                                                                                     *self.roi_feat_size)
+            x_list.append(cur_x)
+        x = torch.cat(x_list)
+        return super(TCTBBoxHead, self).forward(x)
